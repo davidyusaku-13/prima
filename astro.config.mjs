1bd1c60afc909dc58641de1520753d7775504d84
David Yusaku Setiyono <davidyusaku13@gmail.com>
2026-02-20 14:18:06 +0700

    chore: add tailscale funnel endpoint to allowedHosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import node from "@astrojs/node";
 import clerk from "@clerk/astro";
 
 export default defineConfig({
+  server: {
+    host: true,
+    allowedHosts: ["tuf-pc.tail977401.ts.net"],
+  },
   integrations: [clerk()],
   adapter: node({ mode: "standalone" }),
   output: "server",
